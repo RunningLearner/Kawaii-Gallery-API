980261bb7099f71192b7cc403233e48b6029f9c7
RunningLearner <tkacnsdms@naver.com>
2024-08-06 22:08:51 +0900

    feat: 앱 초기화 로직 추가

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
+from dataclasses import asdict
+from typing import Optional
+
+import uvicorn
 from fastapi import FastAPI
+from app.database.conn import db
+from app.common.config import conf
+from app.routes import index
 
-app = FastAPI()
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
+    c = conf()
+    app = FastAPI()
+    conf_dict = asdict(c)
+    db.init_app(app, **conf_dict)
+     # 데이터 베이스 이니셜라이즈
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+    # 레디스 이니셜라이즈
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+    # 미들웨어 정의
+
+    # 라우터 정의
+    app.include_router(index.router)
+    return app
+
+    
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
