2d36ff4f99a0ce69350040cebef091df198957e5
RunningLearner <tkacnsdms@naver.com>
2024-09-03 21:56:17 +0900

    feat: redis 연결

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from dataclasses import asdict
 
-import uvicorn
-import os
-from fastapi import FastAPI
+import redis.asyncio as aioredis
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from app.utils.settings import UPLOAD_DIRECTORY
 from app.database.conn import db
@@ -10,6 +9,7 @@ from app.common.config import conf
 from app.routes import index, auth, posts
 from contextlib import asynccontextmanager
 from app.utils.like_notification import send_like_notification_test
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -21,8 +21,11 @@ async def lifespan(app: FastAPI):
 
     # 데이터 베이스 이니셜라이즈
     db.init_app(app, **conf_dict)  # 먼저 init_app을 호출하여 설정 값을 초기화
-
     await db.connect()
+
+    # Redis 클라이언트 초기화
+    redis_client = aioredis.Redis(host="localhost", port=6379, db=0)
+    app.state.redis = redis_client  # 애플리케이션의 상태에 Redis 클라이언트 저장
 
     # 미들웨어 정의
 
@@ -31,10 +34,13 @@ async def lifespan(app: FastAPI):
     app.include_router(index.router)
     app.include_router(auth.router, tags=["Authentication"], prefix="/api")
     app.include_router(posts.router, tags=["Post"], prefix="/api")
-    
+
     # 정적 파일 제공 경로 매핑
     app.mount("/static", StaticFiles(directory=UPLOAD_DIRECTORY), name="static")
+
     yield
+
+    await redis_client.close()
     await db.close()
 
 
@@ -47,8 +53,11 @@ def create_app():
 
     return app
 
+
 app = create_app()
-# send_like_notification_test()
+
+
+# test endpoints
 @app.get("/test-notification")
 def test_notification_endpoint():
     try:
@@ -58,3 +67,22 @@ def test_notification_endpoint():
     except Exception as e:
         # 예외 발생 시 에러 메시지 반환
         print(status_code=500, detail=str(e))
+
+@app.post("/test_set_redis")
+async def set_redis_data(request: Request):
+    data = await request.json()
+    key = data.get("key")
+    value = data.get("value")
+    print(key)
+    await app.state.redis.set(key, value)
+    return {"message": "Data set successfully", "key": key, "value": value}
+
+@app.get("/test_get_redis")
+async def get_redis_data(key: str):
+    print(key)
+    value = await app.state.redis.get(key)
+    print(value)
+    if value:
+        return {"key": key, "value": value.decode("utf-8")}
+    else:
+        return {"message": "Key not found"}
