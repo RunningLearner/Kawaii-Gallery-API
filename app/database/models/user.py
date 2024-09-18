997333868cee5cc799acff46be6d8330a353d584
RunningLearner <tkacnsdms@naver.com>
2024-09-18 16:44:45 +0900

    fix: 프로필 이미지를 옵셔널로 수정

--- app/database/models/user.py
+++ app/database/models/user.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from odmantic import Field, Model
 
 from app.utils.time_util import get_current_time
@@ -10,6 +11,6 @@ class User(Model):
     feather: int = 0  # 사용자의 깃털 개수
     is_admin: bool = False  # 관리자 여부, 기본값 False
     created_at: datetime = Field(default_factory=get_current_time) # 생성 시간
-    profile_image_url: str  # 파일 url 저장 필드
+    profile_image_url: Optional[str] = None # 파일 url 저장 필드
 
     model_config = {"collection": "users"}
