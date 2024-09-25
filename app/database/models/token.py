ce4abc01824a3981ba112b824e32c4fc9bf1bbe1
RunningLearner <tkacnsdms@naver.com>
2024-09-25 21:29:48 +0900

    feat: 불필요한 열 속성 제거

--- app/database/models/token.py
+++ app/database/models/token.py
@@ -6,7 +6,6 @@ from app.utils.time_util import get_current_time
 class FCMToken(Model):
     user_id: ObjectId
     fcm_token: str
-    device_type: str  # 기기 유형 (예: "android", "ios")
     created_at: datetime = Field(default_factory=get_current_time) # 생성 시간
  
     model_config = {"collection": "fcm_tokens"}
