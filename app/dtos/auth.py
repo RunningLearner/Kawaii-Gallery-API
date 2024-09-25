514fa6660569cdf86de4c3878889b25a42432d6e
RunningLearner <tkacnsdms@naver.com>
2024-09-25 21:26:34 +0900

    refactor: 디바이스 타입 제거

--- app/dtos/auth.py
+++ app/dtos/auth.py
@@ -5,4 +5,3 @@ class FCMTokenCreate(BaseModel):
     FCM(푸시 알림) 토큰을 등록할 때 필요한 데이터 모델입니다.
     """
     fcm_token: str = Field(..., description="Firebase Cloud Messaging(FCM) 토큰", example="example_fcm_token")
-    device_type: str = Field(..., description="디바이스 유형 ('android' 또는 'ios')", example="android")
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -21,25 +21,35 @@ async def kakao_login(
     access_token: str = Body(..., embed=True),
     engine: AIOEngine = Depends(get_mongo_engine),
 ):
-    # 이메일 정보
-    email = get_user_email(access_token)
+    try:
+        # 이메일 정보
+        email = get_user_email(access_token)
 
-    if not email:
-        raise HTTPException(status_code=400, detail="Email not provided by Kakao")
+        if not email:
+            raise HTTPException(status_code=400, detail="Email not provided by Kakao")
 
-    # 기존 회원 체크
-    existing_user = await engine.find_one(User, {"$or": [{"email": email}]})
+        # 기존 회원 체크
+        existing_user = await engine.find_one(User, {"$or": [{"email": email}]})
 
-    if not existing_user:
+        if not existing_user:
+            raise HTTPException(
+                status_code=404, detail="회원가입이 필요합니다. 닉네임을 입력해주세요."
+            )
+
+        # JWT 생성
+        jwt_access_token = create_access_token(data={"user_id": str(existing_user.id)})
+
+        return {"access_token": jwt_access_token}
+
+    except HTTPException as http_ex:
+        logger.error(f"카카오 로그인 실패: {str(http_ex)}", exc_info=True)
+        raise http_ex
+    except Exception as ex:
+        logger.error(f"카카오 로그인 중 서버 오류 발생", exc_info=True)
         raise HTTPException(
-            status_code=404, detail="회원가입이 필요합니다. 닉네임을 입력해주세요."
+            status_code=500,
+            detail="서버 내부 오류가 발생했습니다.",
         )
-
-    # JWT 생성
-    jwt_access_token = create_access_token(data={"user_id": str(existing_user.id)})
-
-    # 회원인지 확인 후 토큰 발급
-    return {"access_token": jwt_access_token}
 
 
 @router.post("/register")
@@ -47,31 +57,42 @@ async def register(
     user_info: UserCreate,
     engine: AIOEngine = Depends(get_mongo_engine),
 ):
-    email = get_user_email(user_info.access_token)
+    try:
+        email = get_user_email(user_info.access_token)
 
-    if not email:
-        raise HTTPException(status_code=400, detail="Email not provided by Kakao")
+        if not email:
+            raise HTTPException(status_code=400, detail="Email not provided by Kakao")
 
-    # 닉네임 중복 확인
-    user = await engine.find_one(User, User.nick_name == user_info.nick_name)
-    if user:
-        raise HTTPException(status_code=400, detail="중복된 닉네임입니다.")
+        # 닉네임 중복 확인
+        user = await engine.find_one(User, User.nick_name == user_info.nick_name)
+        if user:
+            raise HTTPException(status_code=400, detail="중복된 닉네임입니다.")
 
-    # 유저 생성
-    user = User(
-        nick_name=user_info.nick_name,
-        email=email,
-    )
+        # 유저 생성
+        user = User(
+            nick_name=user_info.nick_name,
+            email=email,
+        )
 
-    await engine.save(user)
+        await engine.save(user)
 
-    # 유저 생성 로그
-    logger.info(f"유저 생성 완료: 닉네임 - {user.nick_name}, 이메일 - {user.email}")
+        # 유저 생성 로그
+        logger.info(f"유저 생성 완료: 닉네임 - {user.nick_name}, 이메일 - {user.email}")
 
-    # JWT 생성
-    jwt_access_token = create_access_token(data={"user_id": str(user.id)})
+        # JWT 생성
+        jwt_access_token = create_access_token(data={"user_id": str(user.id)})
 
-    return {"access_token": jwt_access_token}
+        return {"access_token": jwt_access_token}
+
+    except HTTPException as http_ex:
+        logger.error(f"회원 가입 실패: {str(http_ex)}", exc_info=True)
+        raise http_ex
+    except Exception as ex:
+        logger.error(f"회원 가입 중 서버 오류 발생", exc_info=True)
+        raise HTTPException(
+            status_code=500,
+            detail="서버 내부 오류가 발생했습니다.",
+        )
 
 
 @router.post("/fcmtoken")
@@ -80,53 +101,71 @@ async def create_fcm_token(
     engine: AIOEngine = Depends(get_mongo_engine),
     user_id: str = Depends(get_current_user_id),
 ):
-    # 사용자 조회
-    user = await engine.find_one(User, User.id == user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+    try:
+        # 사용자 조회
+        user = await engine.find_one(User, User.id == user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
 
-    # 기존 FCM 토큰 확인
-    existing_token = await engine.find_one(
-        FCMToken,
-        FCMToken.user_id == user.id,
-        FCMToken.device_type == token_info.device_type,
-    )
-    # 있다면 토큰 정보만 업데이트
-    if existing_token:
-        existing_token.fcm_token = token_info.fcm_token
-        await engine.save(existing_token)
-    else:
-        # 새로운 FCM 토큰 생성
-        new_token = FCMToken(
-            user_id=user.id,
-            fcm_token=token_info.fcm_token,
-            device_type=token_info.device_type,
+        # 기존 FCM 토큰 확인
+        existing_token = await engine.find_one(
+            FCMToken,
+            FCMToken.user_id == user.id,
         )
-        await engine.save(new_token)
 
-    return {"message": "FCM token saved successfully"}
+        # 있다면 토큰 정보만 업데이트
+        if existing_token:
+            existing_token.fcm_token = token_info.fcm_token
+            await engine.save(existing_token)
+        else:
+            # 새로운 FCM 토큰 생성
+            new_token = FCMToken(
+                user_id=user.id,
+                fcm_token=token_info.fcm_token,
+            )
+            await engine.save(new_token)
+
+        return {"message": "FCM token이 저장되었습니다."}
+
+    except HTTPException as http_ex:
+        logger.error(f"FCM 토큰 저장 실패: {str(http_ex)}", exc_info=True)
+        raise http_ex
+    except Exception as ex:
+        logger.error(f"FCM 토큰 저장 중 서버 오류 발생", exc_info=True)
+        raise HTTPException(
+            status_code=500,
+            detail="서버 내부 오류가 발생했습니다.",
+        )
 
 
 def get_user_email(access_token: str) -> str:
-    print(f"access_token from dundun :{access_token}")
-    # 액세스 토큰으로 사용자 정보 요청
-    user_info_response = requests.get(
-        user_info_url_kakao,
-        headers={
-            "Authorization": f"Bearer {access_token}",
-            "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-    )
-    print(f"statuscode from kakao :{user_info_response.status_code}")
-
-    if user_info_response.status_code != 200:
-        raise HTTPException(
-            status_code=user_info_response.status_code, detail="Failed to get user info"
+    try:
+        # 액세스 토큰으로 사용자 정보 요청
+        user_info_response = requests.get(
+            user_info_url_kakao,
+            headers={
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
         )
 
-    user_info = user_info_response.json()
+        if user_info_response.status_code != 200:
+            raise HTTPException(
+                status_code=user_info_response.status_code,
+                detail="Failed to get user info",
+            )
 
-    # 이메일 정보
-    email = user_info.get("kakao_account", {}).get("email")
+        user_info = user_info_response.json()
 
-    return email
+        # 이메일 정보
+        return user_info.get("kakao_account", {}).get("email")
+
+    except HTTPException as http_ex:
+        logger.error(f"사용자 정보 요청 실패: {str(http_ex)}", exc_info=True)
+        raise http_ex
+    except Exception as ex:
+        logger.error(f"사용자 정보 요청 중 서버 오류 발생", exc_info=True)
+        raise HTTPException(
+            status_code=500,
+            detail="서버 내부 오류가 발생했습니다.",
+        )
